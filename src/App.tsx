56f968190a28f83b623f40d79dfa6d94f5ca05f2
csachdeva83 <cherishsachdeva16@gmail.com>
2023-12-27 16:23:22 +0530

    Draft Setup * Handle Key command to use default DOM shortcuts

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,38 @@
+import { Editor, EditorState, RichUtils } from 'draft-js';
+import 'draft-js/dist/Draft.css';
+import { useCallback, useState } from 'react';
 import './App.css';
 
 const App = () => {
-  return (
-    <div className="playground-wrapper">
-      <div className="navbar-container">
-        <h3>
-          Demo editor by <span>Cherish Sachdeva</span>
-        </h3>
-        <button>Save</button>
-      </div>
-      <div className="editor-container"></div>
-    </div>
-  );
+    const [editorState, setEditorState] = useState(() => EditorState.createEmpty());
+
+    const handleKeyCommand = useCallback(
+        (command: string) => {
+            const newState = RichUtils.handleKeyCommand(editorState, command);
+
+            if (newState) {
+                setEditorState(newState);
+                return 'handled';
+            }
+
+            return 'not-handled';
+        },
+        [editorState],
+    );
+
+    return (
+        <div className="playground-wrapper">
+            <div className="navbar-container">
+                <h3>
+                    Demo editor by <span>Cherish Sachdeva</span>
+                </h3>
+                <button>Save</button>
+            </div>
+            <div className="editor-container">
+                <Editor editorState={editorState} onChange={setEditorState} handleKeyCommand={handleKeyCommand} />
+            </div>
+        </div>
+    );
 };
 
 export default App;
