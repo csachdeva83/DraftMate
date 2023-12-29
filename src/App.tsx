7d763a66da22559864fd79420eea7fb4ef26a135
Cherish Sachdeva <77549507+csachdeva83@users.noreply.github.com>
2023-12-30 03:11:04 +0530

    Production Release (#4)
    * Setup Eslint & Pretteir
    
    * Elements Layout (#1)
    
    * Feature/draft js (#2)
    
    * Draft Setup
    * Handle Key command to use default DOM shortcuts
    
    * Basic setup for custom key binding
    
    * Editor
    - Apply header-one, bold, red color & underline
    - # and * not removed
    
    * Remove substring of length from from cursor position and add red color
    
    * Cleaner code for handleKeyCommand
    
    * Remove startOffSet if condition
    
    * Use Unicode Character 'ZERO WIDTH SPACE' to remove space in replace text function
    
    * Remove window.setTimeout & provide type to set
    
    * Make DraftEditor-root to take parent div width and height
    
    * Remove package-lock.json
    
    * Feature/local storage (#3)
    
    * Store and Read editorState from local storage
    
    * Add toaster for UI feedback
    
    * Replace if else with map for customCommand
    
    * Change port to 3000 and apply if condition on savedState.length

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,189 @@
-import { useState } from 'react'
-import './App.css'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {
+    DraftHandleValue,
+    Editor,
+    EditorState,
+    Modifier,
+    RichUtils,
+    convertFromRaw,
+    convertToRaw,
+    getDefaultKeyBinding,
+} from 'draft-js';
+import 'draft-js/dist/Draft.css';
+import { OrderedSet } from 'immutable';
+import { KeyboardEvent, useCallback, useEffect, useRef, useState } from 'react';
+import { toast } from 'sonner';
+import { useLocalStorage } from 'usehooks-ts';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+    const [savedState, setSavedState] = useLocalStorage('content', '');
+    const [editorState, setEditorState] = useState(() => {
+        if (savedState.length > 0) {
+            const parsedString = JSON.parse(savedState);
+            const contentState = convertFromRaw(parsedString);
+            return EditorState.createWithContent(contentState);
+        }
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-        </p>
-    </>
-  )
-}
+        return EditorState.createEmpty();
+    });
+    const [sequence, setSequence] = useState<string>('');
+    const editorRef = useRef<Editor>(null);
 
-export default App
+    useEffect(() => {
+        if (editorRef.current) editorRef.current.focus();
+    }, []);
+
+    const styleMap = {
+        RED: {
+            color: 'red',
+        },
+        HEADING: {
+            fontSize: '2em',
+            fontWeight: 'bold',
+            lineHeight: '1.2',
+        },
+        BOLD: {
+            fontWeight: 'bold',
+        },
+        UNDERLINE: {
+            textDecoration: 'underline',
+        },
+    };
+
+    const customCommandMap = new Map<
+        string,
+        {
+            removeCharacterLength: number;
+            set: OrderedSet<string>;
+        }
+    >([
+        [
+            'heading',
+            {
+                removeCharacterLength: 1,
+                set: OrderedSet<string>(['HEADING']),
+            },
+        ],
+        [
+            'bold',
+            {
+                removeCharacterLength: 2,
+                set: OrderedSet<string>(['BOLD']),
+            },
+        ],
+        [
+            'red',
+            {
+                removeCharacterLength: 2,
+                set: OrderedSet<string>(['RED']),
+            },
+        ],
+        [
+            'underline',
+            {
+                removeCharacterLength: 3,
+                set: OrderedSet<string>(['UNDERLINE']),
+            },
+        ],
+    ]);
+
+    const handleKeyCommand = useCallback(
+        (command: string): DraftHandleValue => {
+            const customCommand = customCommandMap.get(command);
+
+            if (customCommand) {
+                const currentSelection = editorState.getSelection();
+                const contentState = editorState.getCurrentContent();
+                const endOffset = currentSelection.getStartOffset();
+                const startOffset = endOffset - customCommand.removeCharacterLength;
+
+                const newContentState = Modifier.replaceText(
+                    contentState,
+                    currentSelection.merge({
+                        anchorOffset: startOffset,
+                        focusOffset: endOffset,
+                    }),
+                    '\u200B',
+                    customCommand.set,
+                );
+
+                const newEditorState = EditorState.push(editorState, newContentState, 'insert-characters');
+                setEditorState(newEditorState);
+
+                return 'handled';
+            }
+
+            const newState = RichUtils.handleKeyCommand(editorState, command);
+
+            if (newState) {
+                setEditorState(newState);
+                return 'handled';
+            }
+
+            return 'not-handled';
+        },
+        [editorState],
+    );
+
+    const myKeyBindingFn = (event: KeyboardEvent<HTMLInputElement>): string | null => {
+        if (event.key === '#' && sequence === '') {
+            setSequence('#');
+        } else if (event.key === ' ' && sequence === '#') {
+            setSequence('');
+            return 'heading';
+        } else if (event.key === '*' && sequence === '') {
+            setSequence('*');
+        } else if (event.key === ' ' && sequence === '*') {
+            setSequence('');
+            return 'bold';
+        } else if (event.key === '*' && sequence === '*') {
+            setSequence('**');
+        } else if (event.key === ' ' && sequence === '**') {
+            setSequence('');
+            return 'red';
+        } else if (event.key === '*' && sequence === '**') {
+            setSequence('***');
+        } else if (event.key === ' ' && sequence === '***') {
+            setSequence('');
+            return 'underline';
+        } else {
+            setSequence('');
+        }
+
+        return getDefaultKeyBinding(event);
+    };
+
+    const saveEditorState = () => {
+        const convertRaw = convertToRaw(editorState.getCurrentContent());
+        setSavedState(JSON.stringify(convertRaw));
+        toast.success('Your changes are being saved', {
+            style: {
+                background: 'green',
+                color: 'white',
+            },
+        });
+    };
+
+    return (
+        <div className="playground-wrapper">
+            <div className="navbar-container">
+                <h3>
+                    Demo editor by <span>Cherish Sachdeva</span>
+                </h3>
+                <button onClick={saveEditorState}>Save</button>
+            </div>
+            <div className="editor-container">
+                <Editor
+                    customStyleMap={styleMap}
+                    editorState={editorState}
+                    onChange={setEditorState}
+                    handleKeyCommand={handleKeyCommand}
+                    keyBindingFn={myKeyBindingFn}
+                    ref={editorRef}
+                />
+            </div>
+        </div>
+    );
+};
+
+export default App;
